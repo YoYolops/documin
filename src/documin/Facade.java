975504c3c50e0a1c2aef8337c03bebdfa37623b8
yo <yohanhxh@gmail.com>
2023-05-22 15:24:54 -0300

    Minor: Clearing unwanted files

---
+++ src/documin/Facade.java
@@ -0,0 +1,20 @@
+package documin;
+
+// import documin.documento.DocumentoController
+
+public class Facade {
+
+    // private DocumentoController documentoController;
+
+    public Facade() {
+        // // exemplo de chamada no construtor:
+        // this.documentoController = new DocumentoController();
+    }
+
+    public boolean criarDocumento(String titulo) {
+        // // exemplo de chamada a ser implementado
+        //  return this.documentoController.criarDocumento(titulo);
+        return false;
+    }
+
+}
